c193f0f9084a3edb16c9f957215dd9e0a1761bcb
h-spear <80024307+h-spear@users.noreply.github.com>
2022-11-03 00:39:25 +0900

    solve 4 programmers level1 problems

---
+++ fruit_ice_cream.sql
@@ -0,0 +1,10 @@
+-- level1
+-- https://school.programmers.co.kr/learn/courses/30/lessons/133025
+
+SELECT FH.FLAVOR AS FLAVOR
+  FROM FIRST_HALF FH
+  JOIN ICECREAM_INFO II
+    ON FH.FLAVOR = II.FLAVOR
+ WHERE FH.TOTAL_ORDER > 3000 AND
+       II.INGREDIENT_TYPE = 'fruit_based'
+ ORDER BY FH.TOTAL_ORDER DESC;
