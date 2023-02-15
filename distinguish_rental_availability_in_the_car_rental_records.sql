0b35c7293ce116ad6c71003f58ef79a4ed91f1f4
h-spear <80024307+h-spear@users.noreply.github.com>
2023-02-16 03:12:31 +0900

    solve 5 programmers mysql problems

---
+++ distinguish_rental_availability_in_the_car_rental_records.sql
@@ -0,0 +1,2 @@
+-- level3
+-- https://school.programmers.co.kr/learn/courses/30/lessons/157340
---
+++ list_of_cars_with_rental_records.sql
@@ -0,0 +1,10 @@
+-- level3
+-- https://school.programmers.co.kr/learn/courses/30/lessons/157341
+
+SELECT DISTINCT C.CAR_ID AS CAR_ID
+  FROM CAR_RENTAL_COMPANY_CAR C
+  JOIN CAR_RENTAL_COMPANY_RENTAL_HISTORY H
+    ON C.CAR_ID = H.CAR_ID
+ WHERE C.CAR_TYPE = '세단' AND
+       MONTH(H.START_DATE) = 10
+ ORDER BY CAR_ID DESC;
